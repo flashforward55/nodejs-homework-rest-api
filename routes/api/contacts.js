e80e048dc606808f4592b1425d771eded44555f1
Anton Styazhkin <rudana089@gmail.com>
2023-08-13 16:09:29 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,7 +6,7 @@ const contacts = require('../../models/contacts');
 const contactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  phone: Joi.number().required(),
 });
 
 router.get('/', async (req, res, next) => {
