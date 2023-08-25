c5dda8c5ac0bbfb2c452be5d5e975a4dad6a4a64
Anton Styazhkin <rudana089@gmail.com>
2023-08-25 17:12:55 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,30 +5,20 @@ const { joiUsersSchemas } = require('../../schemas');
 
 const usersRouter = express.Router();
 
-usersRouter.post(
-    '/register',
-    mdw.validateEmptyBody,
-    mdw.validateBody(joiUsersSchemas.register),
-    ctrl.register
-);
+usersRouter.post('/register', mdw.validateBody(joiUsersSchemas.register), ctrl.register);
 
-usersRouter.post(
-    '/login',
-    mdw.validateEmptyBody,
-    mdw.validateBody(joiUsersSchemas.login),
-    ctrl.login
-);
+usersRouter.post('/login', mdw.validateBody(joiUsersSchemas.login), ctrl.login);
 
 usersRouter.post('/logout', mdw.authenticate, ctrl.logout);
 
 usersRouter.get('/current', mdw.authenticate, ctrl.current);
 
 usersRouter.patch(
-    '/',
-    mdw.authenticate,
-    mdw.validateEmptyBodySub,
-    mdw.validateBody(joiUsersSchemas.updateSubscription),
-    ctrl.updateSubscription
+  '/',
+  mdw.authenticate,
+  mdw.validateEmptyBodySub,
+  mdw.validateBody(joiUsersSchemas.updateSubscription),
+  ctrl.updateSubscription
 );
 
 module.exports = usersRouter;
