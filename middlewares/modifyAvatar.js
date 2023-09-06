852fdb35a3a69937bf48374ad0067868d5256489
Anton Styazhkin <rudana089@gmail.com>
2023-09-06 13:57:22 +0300

    Update modifyAvatar.js

--- middlewares/modifyAvatar.js
+++ middlewares/modifyAvatar.js
@@ -1,25 +1,25 @@
+// Libs
 const fs = require('fs/promises');
 const Jimp = require('jimp');
+// Helpers
 const { HttpError } = require('../helpers');
 
-const AVATAR_SIZE = 250;
-
 const modifyAvatar = async (req, res, next) => {
-  if (!req.file) {
-    return next(HttpError(400, 'No file provided'));
-  }
+  if (!req.file) return next(HttpError(400, 'No file provided'));
 
   const { path } = req.file;
 
   try {
     const uploadedFile = await Jimp.read(path);
-    uploadedFile.cover(AVATAR_SIZE, AVATAR_SIZE).write(path);
-    await fs.unlink(path);
-    next();
+
+    uploadedFile.cover(250, 250).write(path);
   } catch {
     await fs.unlink(path);
+
     return next(HttpError(400, 'Bad file'));
   }
+
+  next();
 };
 
 module.exports = modifyAvatar;
